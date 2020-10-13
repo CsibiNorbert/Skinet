f77bd88946810a072c433ae5de1c4f32779719e9
Norbert Csibi <cibi_norbert@yahoo.com>
2020-10-13 22:47:42 +0100

    Added lazy module for shop route

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,17 +4,17 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    ShopRoutingModule
   ],
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  exports: [
-    ShopComponent
-  ]
+  // exports: [
+  //   ShopComponent
+  // ] No need to export it, as it is lazy loaded
 })
 export class ShopModule { }
