ccad8c271223b6acdd4750f7d44d8c19ba776b2d
Norbert Csibi <cibi_norbert@yahoo.com>
2020-10-17 18:23:39 +0100

    added carousel to the home page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   declarations: [SharedComponent, PagingHeaderComponent, PagerComponent],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
