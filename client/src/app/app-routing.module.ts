66028b59a0f35d922e671287920e3c0ebd16951f
Norbert Csibi <cibi_norbert@yahoo.com>
2020-10-19 22:09:11 +0100

    Created the checkout module skeleton

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,9 @@ const routes: Routes = [
   {path: 'error', component: ErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'shop', data: {breadcrumb: 'Shop'}, loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: 'basket', data: {breadcrumb: 'Basket'}, loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
+  {path: 'checkout',
+    data: {breadcrumb: 'Checkout'},
+    loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'}, // get them back to the home page, maybe a not-found comp?
 ];
 
